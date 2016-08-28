ebe64a2cc6eaf29c459ff920f0ef373ab0876c3f
Sreenath Viswanathan S <vsrenath@goodmethodsglobal.com>
2016-08-28 21:04:52 +0530

    TestMach added

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'TestMach', message:'Welcome to TestMach' });
+});
+
+router.post('/save', function(req, res, next) {
+  res.render('index', { title: 'TestMach', message:'Welcome to TestMach' });
+});
+
+module.exports = router;
