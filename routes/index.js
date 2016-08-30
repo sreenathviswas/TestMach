1301931b9841335b6b2ff28dd04e82126ebe96a7
Sreenath Viswanathan S <vsrenath@goodmethodsglobal.com>
2016-08-30 09:16:41 +0530

    DB initial check in

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/testmach';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TestMach', model:req.body||{} });
+  res.render('index', { title: 'TestMach', model:req.body||{}, ruleName:[] });
 });
 
-router.post('/', function(req, res, next) {
-  res.render('index', { title: 'TestMach', model:req.body });
+router.post('/', function(req, res, next) {	
+	var cursor = [];
+	MongoClient.connect(url, function(err, db) {
+	  console.log("Connected correctly to server..........");
+	   cursor = db.collection('rule').find();
+		cursor.each(function(err, doc) {
+		  if (doc != null) {
+			 console.dir(doc.ruleName);
+		  } 
+		});
+	  db.close();
+	  res.render('index', { title: 'TestMach', model:req.body, ruleName:cursor });
+	  console.dir(cursor[0]);
+	});	
 });
 
 module.exports = router;
