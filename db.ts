e7763eec9e9b17151dc56d279ce1bda261f31937
Sreenath Viswanathan S <vsrenath@goodmethodsglobal.com>
2016-09-01 07:59:20 +0530

    unique key implementation

--- db.ts
+++ db.ts
@@ -5,7 +5,7 @@ import * as mongodb from 'mongodb';
 
 var server = new mongodb.Server('localhost', 27017);
 var db = new mongodb.Db('testmach', server, { w: 1 });
-db.open(function() {});
+db.open(function () { });
 
 export interface Rule {
     _id: string;
@@ -13,36 +13,39 @@ export interface Rule {
 }
 
 export function getRules(callback: (rules: Rule[]) => void) {
-    db.collection('rule', function(error, rules_collection) {
-        if(error) { console.error(error); return; }
-        rules_collection.find({}, { 'ruleName': 1 }).toArray(function(error, ruleobjs) {
-           if(error) { console.error(error); return; }
-           callback(ruleobjs);
+    db.collection('rule', function (error, rules_collection) {
+        if (error) { console.error(error); return; }
+        rules_collection.find({}, { 'ruleName': 1 }).toArray(function (error, ruleobjs) {
+            if (error) { console.error(error); return; }
+            callback(ruleobjs);
         });
     });
 }
 
-export function addRule(ruleName: string, callback: (rules: Rule[]) => void){
-	db.collection('rule', function(error, rules_collection) {
-        if(error) { console.error(error); return; }
+export function addRule(ruleName: string, callback: () => void) {
+    db.collection('rule', function (error, rules_collection) {
+        if (error) { console.error(error); return; }
         rules_collection.insert({
-			"ruleName" : ruleName
-        }), (function (error, ruleobjs) {
-            console.log("Insert Error", error, ruleobjs);
-           if(error) { console.error(error); return; }
-           callback(ruleobjs);
+            "ruleName": ruleName
+        }, function (error, result) {
+            if (error) {
+                //JSON.stringify(error)
+                throw new Error("Error occurred!");
+            }
+            console.error("calling call back");
+            callback();
         });
     });
 }
 
-export function deleteRule(id: string, callback: () => void){
+export function deleteRule(id: string, callback: () => void) {
     db.collection('rule', {}, function (error, rules_collection) {
         if (error) { console.error(error); return; }
-        rules_collection.remove({ 
+        rules_collection.remove({
             _id: mongodb.ObjectID.createFromHexString(id)
-			}),(function (err, result) {
-				if (err) { console.log("Delete failed:" ,err); }				
-				callback();
+        }), (function (err, result) {
+            if (err) { console.log("Delete failed:", err); }
+            callback();
         });
     });
 }
--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,31 @@ var router = express.Router();
 var db = require("../db");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-	db.getRules(function (rules) {
-		res.render('index', { title: 'TestMach', model:{}, rules: rules });
-	});
+router.get('/', function (req, res, next) {
+    db.getRules(function (rules) {
+        res.render('index', { title: 'TestMach', model: {}, rules: rules });
+    });
 });
 
 router.post('/', function (req, res, next) {
     if (req.body.firstName.length == 0) {
         return res.render('index', { title: 'TestMach', model: { "firstName": "First Name missing" }, rules: JSON.parse(req.body.rules) });
-    }    
-
-	db.addRule(req.body.firstName, function (rules) {
-	});	
-	res.redirect('/');
+    }
+    try {
+        var a = db.addRule(req.body.firstName, function (rules) { });
+        console.log(a);
+    }
+    catch (e) {
+        console.log(e);
+    }
+    res.redirect('/');
 });
 
 
-router.post('/delete', function(req, res, next) {	
-	db.deleteRule(req.body.id, function () {
-	});	
-	res.redirect('/');
+router.post('/delete', function (req, res, next) {
+    db.deleteRule(req.body.id, function () {
+    });
+    res.redirect('/');
 });
 
 module.exports = router;
